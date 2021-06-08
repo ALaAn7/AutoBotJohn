734a141432ee8dfc065f28874f1877516a51cfa9
ALaAn7 <85429481+ALaAn7@users.noreply.github.com>
2021-06-08 11:38:37 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -90,12 +90,12 @@ async def cb_navg(bot, update: CallbackQuery):
     else:
         temp_results.append([
             InlineKeyboardButton("⏪ Back", callback_data=f"navigate({index_val}|back|{query})"),
-            InlineKeyboardButton("🕹️ Next Page 🕹️", callback_data=f"navigate({index_val}|next|{query})")
+            InlineKeyboardButton("⬅️ Next", callback_data=f"navigate({index_val}|next|{query})")
         ])
 
     if not int(index_val) == 0:    
         temp_results.append([
-            InlineKeyboardButton(f"🔰 Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"♻️ Page {index_val + 1}/{len(results) if len(results) < max_pages else max_pages} ♻️", callback_data="ignore")
         ])
     
     if show_invite and int(index_val) !=0 :
